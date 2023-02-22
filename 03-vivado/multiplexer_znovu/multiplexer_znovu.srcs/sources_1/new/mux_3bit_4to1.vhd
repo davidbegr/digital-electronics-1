5d33a114f7e938c92aa890ff57590d8b4727f265
davidbegr <105978401+davidbegr@users.noreply.github.com>
2023-02-22 15:43:33 +0100

    Update mux_3bit_4to1.vhd

--- 03-vivado/multiplexer_znovu/multiplexer_znovu.srcs/sources_1/new/mux_3bit_4to1.vhd
+++ 03-vivado/multiplexer_znovu/multiplexer_znovu.srcs/sources_1/new/mux_3bit_4to1.vhd
@@ -11,19 +11,19 @@ entity mux_3bit_4to1 is
         b           : in  std_logic_vector(2 downto 0);
         c           : in  std_logic_vector(2 downto 0);
         d           : in  std_logic_vector(2 downto 0);
-        f           : in  std_logic_vector(2 downto 0);
-        sel           : in  std_logic_vector(1 downto 0);                                
+        f           : out  std_logic_vector(2 downto 0);
+        sel         : in  std_logic_vector(1 downto 0)
     );
 end entity mux_3bit_4to1;
 
 ------------------------------------------------------------
--- Architecture body for 4-bit binary comparator
+-- Architecture body for MUX
 ------------------------------------------------------------
 architecture Behavioral of mux_3bit_4to1 is
 begin
     with sel select
     f <= a when "00",
-         b when "01",
-         c when "10",
-         d when others;
+    	 b when "01",
+    	 c when "10",
+    	 d when others;
 end architecture Behavioral;
